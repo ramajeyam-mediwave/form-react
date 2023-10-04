e38798e8d777865092ebc84f83d715c31bb35827
ramajeyam-mediwave <ramajeyam@mindwaveventures.com>
2023-10-04 16:32:25 +0530

    started

---
+++ README.md
@@ -0,0 +1,1 @@
+# Creating form using react components
---
+++ app/src/App.tsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import { Input, Button } from "./Components";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Input
+          name="First Name"
+          type="text"
+          placeholder="Enter your First name"
+        />
+        <Input
+          name="Second Name"
+          type="text"
+          placeholder="Enter your Second name"
+        />
+        <Input name="Your Age" type="number" placeholder="Enter your age" />
+        <Button/>
+      </header>
+    </div>
+  );
+}
+export default App;
